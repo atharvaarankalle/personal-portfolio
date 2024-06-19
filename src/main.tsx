4d5423ba88b0f92693bbc8837c13217417dbccd4
Atharva Arankalle <atharvanz@gmail.com>
2024-06-19 16:36:17 +1200

    :sparkles: (landing-page): added initial page and routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
