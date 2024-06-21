ad35b85ea8fa66b4284dd119d3e2dfff7604733c
Atharva Arankalle <atharvanz@gmail.com>
2024-06-21 17:10:38 +1200

    ✨ feat(socials-buttons): added tooltips to buttons

--- src/components/SocialsButtons.tsx
+++ src/components/SocialsButtons.tsx
@@ -1,9 +1,10 @@
-import { IconButton, Stack, styled } from "@mui/material";
+import { IconButton, Stack, Typography, styled } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
 import DescriptionIcon from "@mui/icons-material/Description";
 import resume from "../assets/Atharva Arankalle Resume.pdf";
+import StyledTooltip from "./StyledTooltip";
 
 const StyledIconButton = styled(IconButton)(({ theme }) => ({
   padding: "0.2rem",
@@ -19,33 +20,54 @@ const SocialsButtons = () => {
 
   return (
     <Stack direction="row" gap={2}>
-      <StyledIconButton
-        aria-label="linkedin"
-        href="https://www.linkedin.com/in/atharva-arankalle/"
-        target="_blank"
+      <StyledTooltip
+        title={<Typography sx={{ fontSize: "1rem" }}>LinkedIn</Typography>}
+        placement="bottom"
+        arrow
+        disableInteractive
       >
-        <LinkedInIcon sx={{ color: "#FFFFFF" }} fontSize="large" />
-      </StyledIconButton>
-      <StyledIconButton
-        aria-label="github"
-        href="https://github.com/atharvaarankalle"
-        target="_blank"
-      >
-        <GitHubIcon sx={{ color: "#FFFFFF" }} fontSize="large" />
-      </StyledIconButton>
-      <StyledIconButton
-        aria-label="email"
-        onClick={openMailClient}
-      >
-        <EmailIcon sx={{ color: "#FFFFFF" }} fontSize="large" />
-      </StyledIconButton>
-      <StyledIconButton
-        aria-label="resume"
-        href={resume}
-        target="_blank"
+        <StyledIconButton
+          aria-label="linkedin"
+          href="https://www.linkedin.com/in/atharva-arankalle/"
+          target="_blank"
         >
-        <DescriptionIcon sx={{ color: "#FFFFFF" }} fontSize="large" />  
-        </StyledIconButton>        
+          <LinkedInIcon sx={{ color: "#FFFFFF" }} fontSize="large" />
+        </StyledIconButton>
+      </StyledTooltip>
+      <StyledTooltip
+        title={<Typography sx={{ fontSize: "1rem" }}>GitHub</Typography>}
+        placement="bottom"
+        arrow
+        disableInteractive
+      >
+        <StyledIconButton
+          aria-label="github"
+          href="https://github.com/atharvaarankalle"
+          target="_blank"
+        >
+          <GitHubIcon sx={{ color: "#FFFFFF" }} fontSize="large" />
+        </StyledIconButton>
+      </StyledTooltip>
+      <StyledTooltip
+        title={<Typography sx={{ fontSize: "1rem" }}>Email</Typography>}
+        placement="bottom"
+        arrow
+        disableInteractive
+      >
+        <StyledIconButton aria-label="email" onClick={openMailClient}>
+          <EmailIcon sx={{ color: "#FFFFFF" }} fontSize="large" />
+        </StyledIconButton>
+      </StyledTooltip>
+      <StyledTooltip
+        title={<Typography sx={{ fontSize: "1rem" }}>CV</Typography>}
+        placement="bottom"
+        arrow
+        disableInteractive
+      >
+        <StyledIconButton aria-label="resume" href={resume} target="_blank">
+          <DescriptionIcon sx={{ color: "#FFFFFF" }} fontSize="large" />
+        </StyledIconButton>
+      </StyledTooltip>
     </Stack>
   );
 };
