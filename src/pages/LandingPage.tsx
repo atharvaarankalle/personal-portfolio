e530406fad598ab9da15be035f9dbedb5f2fe2d9
Atharva Arankalle <atharvanz@gmail.com>
2024-06-21 17:21:16 +1200

    🐛 fix(landing-page): fixed typescript error

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,12 @@
 import { Box, styled } from "@mui/material";
 import ProfileSummary from "../components/ProfileSummary";
 
-const StyledBox = styled(Box)(({ theme }) => ({
+const StyledBox = styled(Box)({
   display: "flex",
   justifyContent: "center",
   height: "100%",
   alignItems: "center"
-}));
+});
 
 const LandingPage = () => {
   return (
