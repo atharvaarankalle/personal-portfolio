99a2499654d7ac25d516b6c1e94f9ad92081618e
Atharva Arankalle <atharvanz@gmail.com>
2024-06-23 15:30:00 +1200

    ✨ feat(data): added urls for badges

---
+++ src/data/BadgeUrls.ts
@@ -0,0 +1,24 @@
+const BadgeUrls: { [key: string]: string } = {
+    "C#": "https://img.shields.io/badge/C%23-%2338008F?style=for-the-badge",
+    Confluence: "https://img.shields.io/badge/Confluence-%23172B4D?style=for-the-badge&logo=confluence&logoColor=%23FFFFFF",
+    CSS: "https://img.shields.io/badge/CSS-%231572B6?style=for-the-badge&logo=css3&logoColor=%23FFFFFF",
+    Express: "https://img.shields.io/badge/Express-%23000000?style=for-the-badge&logo=express",
+    Firebase: "https://img.shields.io/badge/Firebase-%23DD2C00?style=for-the-badge&logo=firebase&logoColor=%23FFFFFF",
+    Git: "https://img.shields.io/badge/Git-%23F05032?style=for-the-badge&logo=git&logoColor=%23FFFFFF",
+    GitHub: "https://img.shields.io/badge/GitHub-%23181717?style=for-the-badge&logo=github&logoColor=%23FFFFFF",
+    GitLab: "https://img.shields.io/badge/GitLab-%23FC6D26?style=for-the-badge&logo=gitlab&logoColor=%23FFFFFF",
+    HTML: "https://img.shields.io/badge/HTML-%23E34F26?style=for-the-badge&logo=html5&logoColor=%23FFFFFF",
+    Java: "https://img.shields.io/badge/Java-%23f39633?style=for-the-badge",
+    JavaScript: "https://img.shields.io/badge/JavaScript-%23F7DF1E?style=for-the-badge&logo=javascript&logoColor=%23000000",
+    Jira: "https://img.shields.io/badge/Jira-%230052CC?style=for-the-badge&logo=jira&logoColor=%23FFFFFF",
+    "Material UI": "https://img.shields.io/badge/Material%20UI-%23007FFF?style=for-the-badge&logo=mui&logoColor=%23FFFFFF",
+    MongoDB: "https://img.shields.io/badge/MongoDB-%2347A248?style=for-the-badge&logo=mongodb&logoColor=%23FFFFFF",
+    "Node.js": "https://img.shields.io/badge/Node.js-%235FA04E?style=for-the-badge&logo=nodedotjs&logoColor=%23FFFFFF",
+    Python: "https://img.shields.io/badge/Python-%233776AB?style=for-the-badge&logo=python&logoColor=%23FFFFFF",
+    React: "https://img.shields.io/badge/React-%230088CC?style=for-the-badge&logo=react",
+    SQL: "https://img.shields.io/badge/SQL-%234479A1?style=for-the-badge&logo=mysql&logoColor=%23FFFFFF",
+    TypeScript: "https://img.shields.io/badge/TypeScript-%233178C6?style=for-the-badge&logo=typescript&logoColor=%23FFFFFF",
+    Vue: "https://img.shields.io/badge/Vue-%234FC08D?style=for-the-badge&logo=vuedotjs&logoColor=%23FFFFFF"
+};
+
+export default BadgeUrls;
