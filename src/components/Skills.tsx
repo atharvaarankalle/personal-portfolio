81ec65aae3ea14d27a108054a60ba67dc11d6a22
Atharva Arankalle <atharvanz@gmail.com>
2024-06-23 15:31:11 +1200

    ✨ feat(skills-section): added skills section to website

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,5 @@
 import { Box, Card, CardContent, Typography, styled } from "@mui/material";
+import TechnologyChip from "./TechnologyChip";
 
 const StyledBox = styled(Box)({
     width: "100%",
@@ -16,16 +17,47 @@ const StyledCard = styled(Card)(({ theme }) => ({
   borderRadius: 10,
   boxShadow: "0 0 20px rgba(0, 0, 0, 0.1)",
   display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
+  flexDirection: "column"
 }));
+
+const frontendSkills = ["HTML", "CSS", "JavaScript", "TypeScript", "React", "Vue", "Material UI"];
+
+const backendSkills = ["Node.js", "Express", "Python", "Java", "C#"];
+
+const dataSkills = ["SQL", "MongoDB", "Firebase"];
+
+const toolsSkills = ["Git", "GitHub", "GitLab", "Jira", "Confluence"];
 
 const Skills = () => {
     return (
       <StyledBox id="skills">
         <StyledCard>
           <CardContent>
-            <Typography variant="h3" fontWeight="bold">Skills</Typography>
+            <Typography variant="h3" fontWeight="bold" sx={{ paddingBottom: "1rem" }}>Skills</Typography>
+            <Typography variant="h5" fontWeight="bold" color="secondary">Frontend</Typography>
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: "1rem", paddingTop: "0.75rem" }}>
+              {frontendSkills.map((skill) => (
+                <TechnologyChip key={skill} technologyName={skill} />
+              ))}
+            </Box>
+            <Typography variant="h5" fontWeight="bold" color="secondary" sx={{ paddingTop: "2rem" }}>Backend</Typography>
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: "1rem", paddingTop: "0.75rem" }}>
+              {backendSkills.map((skill) => (
+                <TechnologyChip key={skill} technologyName={skill} />
+              ))}
+            </Box>
+            <Typography variant="h5" fontWeight="bold" color="secondary" sx={{ paddingTop: "2rem" }}>Data</Typography>
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: "1rem", paddingTop: "0.75rem" }}>
+              {dataSkills.map((skill) => (
+                <TechnologyChip key={skill} technologyName={skill} />
+              ))}
+            </Box>
+            <Typography variant="h5" fontWeight="bold" color="secondary" sx={{ paddingTop: "2rem" }}>Tools</Typography>
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: "1rem", paddingTop: "0.75rem" }}>
+              {toolsSkills.map((skill) => (
+                <TechnologyChip key={skill} technologyName={skill} />
+              ))}
+            </Box>
           </CardContent>
         </StyledCard>
       </StyledBox>
